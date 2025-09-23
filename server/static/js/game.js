1080c55354b66fc4d52ab1e4ec36111881ea0d4b
Robin-Sommer <robin.sommer@otto.de>
2025-09-23 13:26:39 +0200

    Add loading in new products

--- server/static/js/game.js
+++ server/static/js/game.js
@@ -1,55 +1,103 @@
+var arrow = document.getElementById('arrow');
+var logobig = document.getElementById('logobig');
+var arrow_container = document.getElementsByClassName('arrow-container')[0]
+var product1 = document.getElementsByClassName('product-box')[0];
+var product2 = document.getElementsByClassName('product-box')[1];
+
 document.addEventListener("DOMContentLoaded", (event) => {
-
-const arrow = document.getElementById('arrow');
-const questionMark = document.getElementById('logobig');
-const product1 = document.getElementById('product1');
-const product2 = document.getElementById('product2');
-
-
-// Maus berührt ein Feld
-product1.addEventListener('mouseenter', () => {
-    arrow.style.transform = 'rotate(-180deg)';
-    arrow.style.display = 'block';
-    logobig.style.display = 'none';
+    setup();
 });
 
-product2.addEventListener('mouseenter', () => {
-    arrow.style.transform = 'rotate(0deg)';
-    arrow.style.display = 'block';
-    logobig.style.display = 'none';
-});
-
-// Maus verlässt ein Feld
-product1.addEventListener('mouseleave', () => {
-    if (!product2.matches(':hover')) { // Wenn Maus NICHT über product2 ist
-        arrow.style.transform = 'rotate(-90deg)';
-        arrow.style.display = 'block';
-        logobig.style.display = 'none';
-    }
-});
-
-product2.addEventListener('mouseleave', () => {
-    if (!product1.matches(':hover')) { // Wenn Maus NICHT über product1 ist
-        arrow.style.transform = 'rotate(-90deg)';
-        arrow.style.display = 'block';
-        logobig.style.display = 'none';
-    }
-});
-
-function send_guess(user_guess) {
-    response = await fetch("/guess", {
+async function send_guess() {
+    await fetch("/guess", {
         method: 'POST',
+        redirect: 'follow',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ guess: user_guess })
-    })
-
-
+        body: JSON.stringify({ guess: this.getAttribute('user_guess') })
+    }).then(response => {
+        console.log(response.status)
+        if (response.redirected) {
+            window.location.href = response.url;
+        }
+        return response.json();
+    }).then(response => {
+        console.log(response);
+        console.log(response.productLast_price);
+        load_next_product(response);
+    });
 }
 
-// Initialzustand
-arrow.style.display = 'none';
-logobig.style.display = 'block';
+function load_next_product(response) {
+    product1.remove();
+    moveDown(arrow_container);
+    product2.removeEventListener('mouseenter', enter_rotate);
+    product2.removeEventListener('mouseleave', leave_rotate);
+    product2.removeEventListener('click', send_guess);
+    document.querySelector('.price').textContent = `Preis: ${response.productLast_price} €`
 
-});
+    document.querySelector('.product-container').insertAdjacentHTML('beforeend', `<div class="product-box">
+                    <img class="product-image" src=${response.productNext_high_q_img} alt="Produkt">
+                    <div class="text-box">
+                        <h2>${response.productNext_brand}</h2>
+                        <h3>${response.productNext_name}</h3>
+                        <p class="price">Preis: ${response.productNext_price} €</p>
+                        <p>lieferbar - in 2-3 Werktagen bei dir</p>
+                        <img alt="Logo" class="UpLogo" src="/static/images/otto-up-logo.png">
+                    </div>
+                </div>`);
+
+    setup();
+}
+
+function moveDown(element) {
+  if(element.nextElementSibling)
+    element.parentNode.insertBefore(element.nextElementSibling, element);
+}
+
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function setup() {
+    arrow = document.getElementById('arrow');
+    logobig = document.getElementById('logobig');
+    arrow_container = document.getElementsByClassName('arrow-container')[0]
+    product1 = document.getElementsByClassName('product-box')[0];
+    product2 = document.getElementsByClassName('product-box')[1];
+
+    product1.setAttribute('rotation', '-180');
+    product2.setAttribute('rotation', '0');
+
+    product1.setAttribute('user_guess', 'lower');
+    product2.setAttribute('user_guess', 'higher');
+
+    // Initialzustand
+    arrow.style.display = 'none';
+    logobig.style.display = 'block';
+
+    // Maus berührt ein Feld
+    product1.addEventListener('mouseenter', enter_rotate);
+    product2.addEventListener('mouseenter', enter_rotate);
+
+    // Maus verlässt ein Feld
+    product1.addEventListener('mouseleave', leave_rotate);
+    product2.addEventListener('mouseleave', leave_rotate);
+
+    product1.addEventListener('click', send_guess);
+    product2.addEventListener('click', send_guess);
+}
+
+function enter_rotate() {
+    rotation = this.getAttribute('rotation')
+    arrow.style.transform = `rotate(${rotation}deg)`;
+    arrow.style.display = 'block';
+    logobig.style.display = 'none';
+}
+
+function leave_rotate() {
+    arrow.style.transform = 'rotate(-90deg)';
+    arrow.style.display = 'block';
+    logobig.style.display = 'none';
+}
