a2aacfaf5247518b551c4a514cef165688d7a9aa
Robin-Sommer <robin.sommer@otto.de>
2025-09-24 15:38:25 +0200

    Show one board

--- server/static/js/index_script.js
+++ server/static/js/index_script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  difficulties = document.querySelectorAll(".difficulty input");
-  leaderboards = document.querySelectorAll(".leaderboard-container")
-
-  difficulties.forEach((e, i) => {if (e.checked) leaderboards[i].classList.add("hidden")})
+    difficulties = document.querySelectorAll(".difficulty input");
+    show_selected_board();
 });
+
+function show_selected_board() {
+    difficulties = document.querySelectorAll(".difficulty input");
+    leaderboards = document.querySelectorAll(".leaderboard-container")
+
+    leaderboards.forEach((e) => e.classList.add("hidden"))
+    difficulties.forEach((e, i) => {if (e.checked) leaderboards[i].classList.remove("hidden")})
+}
